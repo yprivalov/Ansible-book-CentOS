8d26b0888d6c3e9e755b679be6c3ef9c5c6d26b5
JurPri <mail@privalov.info>
2019-06-10 13:04:41 +0300

    Update docker-compose-install.sh

--- docker-compose-install.sh
+++ docker-compose-install.sh
@@ -4,13 +4,13 @@
 
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce
+yum install -y docker-ce
 systemctl enable docker.service
 systemctl start docker.service
 
 # install docker-compose
 
-yum install epel-release
+yum install -y epel-release
 yum install -y python-pip
 pip install docker-compose
 yum upgrade python*
